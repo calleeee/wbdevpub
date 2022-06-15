5087c344af5d2cd4bfae11b1337e9576dd2ce59d
calleeee <94118178+calleeee@users.noreply.github.com>
2022-06-15 19:53:30 +0200

    Add files via upload

---
+++ my-svelte-project/src/videos.js
@@ -0,0 +1,22 @@
+export const videos = [
+    {
+        "poster": "https://image.tmdb.org/t/p/w500/56XAyRDe8UpeAb7QKdqyERpXhTM.jpg",
+        "src": "https://sveltejs.github.io/assets/caminandes-llamigos.mp4"
+    },
+    {
+        "poster": "https://sfanytime-images-prod-http2.secure2.footprint.net/COVERM/COVERM_c784345b-a05d-42fd-bb69-0daea5b6330a_sv.jpg?w=375&ar=0.692&fit=crop&fm=pjpg&s=b5ee2e905776ab966d2ea19236d818c4",
+        "src": "https://shattereddisk.github.io/rickroll/rickroll.mp4"
+    },
+    {
+        "poster": "https://m.media-amazon.com/images/M/MV5BMmIwZDMyYWUtNTU0ZS00ODJhLTg2ZmEtMTk5ZmYzODcxODYxXkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_.jpg",
+        "src": "http://techslides.com/demos/sample-videos/small.mp4"
+    },
+    {
+        "poster": "https://lumiere-a.akamaihd.net/v1/images/p_cars_19643_4405006d.jpeg",
+        "src": "http://techslides.com/demos/sample-videos/small.mp4"
+    },
+    {
+        "poster": "https://upload.wikimedia.org/wikipedia/en/b/bb/You_Season_2.jpg",
+        "src": "http://techslides.com/demos/sample-videos/small.mp4"
+    }
+]
